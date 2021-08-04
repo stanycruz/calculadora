1bce6da10988b062b5ed1e65bac806360ea7c914
Stanislaw Cruz <stanycruz@outlook.com>
2021-08-04 17:40:10 -0300

    Componente Button #01

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calculator />
+    <div>
+      <h1>Calculadora</h1>
+      <Calculator />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
